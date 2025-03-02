85c04fc2677c18a116c371f9473624750268f9a9
Sephens <adesephens@gmail.com>
2025-03-02 20:13:41 +0300

    breaking the preproInsulin with python slicing

---
+++ test.py
@@ -0,0 +1,17 @@
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+# breaking the preproInsulin
+lsInsulin = preproInsulin[:24] # 24
+print(lsInsulin)
+
+bInsulinoInsulin =preproInsulin[24:54] # 30
+print(bInsulinoInsulin)
+
+cInsulin = preproInsulin[54:89] # 35
+print(cInsulin)
+
+aInsulin = preproInsulin[89:] # 21
+print(aInsulin)
+
+
